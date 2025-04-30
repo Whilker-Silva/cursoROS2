c1efe3ea7d3251747e7d3a263091452d8a672bed
Whilker Henrique dos Santos Silva <whilker_santos@hotmail.com>
2025-04-30 08:01:21 -0300

    .

--- src/ros_px4_cpp/src/Trajetoria_fixa/Trajetoria_fixa.cpp
+++ src/ros_px4_cpp/src/Trajetoria_fixa/Trajetoria_fixa.cpp
@@ -3,59 +3,69 @@
 Trajetoria_fixa::Trajetoria_fixa(string name, string ns, NodeOptions options) : Node(name, ns, options)
 {
     QoS qos_profile(KeepLast(10));
-    qos_profile.durability(DurabilityPolicy::TransientLocal); // Setando para BEST_EFFORT
+    qos_profile.reliability(ReliabilityPolicy::Reliable);
+    qos_profile.durability(DurabilityPolicy::TransientLocal);
+
+    subscription = this->create_subscription<std_msgs::msg::Bool>("/decolou", 10, bind(&Trajetoria_fixa::dronePronto_callback, this, _1));
 
     publisherVelocity = this->create_publisher<geometry_msgs::msg::Twist>("/offboard_velocity_cmd", qos_profile);
     publisherAmr = this->create_publisher<std_msgs::msg::Bool>("/arm_message", qos_profile);
 
-    RCLCPP_INFO(this->get_logger(), "control has been started");
+    this->decolarDrone();
 
-    this->moveDrone();
-    RCLCPP_INFO(this->get_logger(), "control has been finished");      
+    RCLCPP_INFO(this->get_logger(), "control has been started");
 }
 
 Trajetoria_fixa::~Trajetoria_fixa()
 {
-    
+}
+
+void Trajetoria_fixa::dronePronto_callback(const std_msgs::msg::Bool::SharedPtr msg)
+{
+    if (msg->data == true)
+    {
+        RCLCPP_INFO(this->get_logger(), "Drone decolou e está estável");
+        sleep_for(3s);
+        this->moveDrone();
+    }
+}
+
+void Trajetoria_fixa::decolarDrone()
+{
+    auto arm = std_msgs::msg::Bool();
+    arm.data = true;
+    publisherAmr->publish(arm);
 }
 
 void Trajetoria_fixa::moveDrone()
 {
     auto msg = geometry_msgs::msg::Twist();
-    auto arm = std_msgs::msg::Bool();
 
-    arm.data=true;
-    publisherAmr->publish(arm);
-    sleep_for(20s);
-    RCLCPP_INFO(this->get_logger(), "Decolouuu");
+    
 
     msg.linear.x = 2;
-    msg.linear.y = 0;
-    msg.linear.z = 0;
+    msg.linear.y = 0;    
     publisherVelocity->publish(msg);
+    RCLCPP_INFO(this->get_logger(), "(2,0)");
     sleep_for(3s);
 
     msg.linear.x = 2;
-    msg.linear.y = 2;
-    msg.linear.z = 0;
+    msg.linear.y = 2;    
     publisherVelocity->publish(msg);
+    RCLCPP_INFO(this->get_logger(), "(2,2)");
     sleep_for(3s);
 
     msg.linear.x = 0;
-    msg.linear.y = 2;
-    msg.linear.z = 0;
+    msg.linear.y = 2;   
     publisherVelocity->publish(msg);
+    RCLCPP_INFO(this->get_logger(), "(0,2)");
     sleep_for(3s);
 
     msg.linear.x = 0;
-    msg.linear.y = 0;
-    msg.linear.z = 0;
+    msg.linear.y = 0;   
     publisherVelocity->publish(msg);
+    RCLCPP_INFO(this->get_logger(), "(0,0)");
     sleep_for(3s);
-
-
-    arm.data=false;
-    publisherAmr->publish(arm);
 }
 
 int main(int argc, char **argv)
@@ -63,7 +73,7 @@ int main(int argc, char **argv)
     NodeOptions options;
     init(argc, argv);                                                                 // inicializado o ROS
     auto node = make_shared<Trajetoria_fixa>("trajetoria", "/px4_offboard", options); // criação do nó usando ponteiro compartilhado e o nomeando
-    //spin(node);                                                                       // gira o nó (o mantem vivo até ctrl + c)
+    spin(node);                                                                       // gira o nó (o mantem vivo até ctrl + c)
     shutdown();                                                                       // desliga o nó
     return 0;
 }
--- src/ros_px4_cpp/src/Trajetoria_fixa/Trajetoria_fixa.h
+++ src/ros_px4_cpp/src/Trajetoria_fixa/Trajetoria_fixa.h
@@ -7,6 +7,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "std_msgs/msg/bool.hpp"
 
+
 using namespace std;
 using namespace rclcpp;
 
@@ -15,10 +16,19 @@ using placeholders::_1;
 class Trajetoria_fixa : public Node
 {
 private:
+
+    Subscription<std_msgs::msg::Bool>::SharedPtr subscription;
+
     Publisher<geometry_msgs::msg::Twist>::SharedPtr publisherVelocity;
     Publisher<std_msgs::msg::Bool>::SharedPtr publisherAmr;
+
     TimerBase::SharedPtr timer;
-    void moveDrone();
+
+    bool decolou;
+
+    void dronePronto_callback(const std_msgs::msg::Bool::SharedPtr msg);
+    void decolarDrone();    
+    void moveDrone();    
    
 public:
     Trajetoria_fixa(string name, string ns, NodeOptions options);
